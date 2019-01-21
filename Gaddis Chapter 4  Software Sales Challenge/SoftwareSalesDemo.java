fc020449ffdd5d40d9e93a394b25b21913f8998b
Jenny Conner <38302529+jennyswhitelist@users.noreply.github.com>
2019-01-21 09:41:44 -0800

    Updated comments

---
+++ Gaddis Chapter 4  Software Sales Challenge/SoftwareSalesDemo.java
@@ -0,0 +1,32 @@
+package chapter4;
+
+
+import java.text.DecimalFormat;
+import java.util.Scanner;
+
+/**
+ * SoftwareSalesDemo Program
+ */
+
+public class SoftwareSalesDemo {
+	public static void main(String[] args) {
+		int unitsSold; // Holds units sold
+		
+
+		// Create a Scanner object for keyboard input.
+		Scanner keyboard = new Scanner(System.in);
+
+		// Get the units sold.
+		System.out.print("Enter the units sold: ");
+		unitsSold = keyboard.nextInt();
+
+		// Create a SoftwareSales object.
+		SoftwareSales sales = new SoftwareSales(unitsSold);
+
+		// Display purchase info.
+		DecimalFormat dollar = new DecimalFormat("#,##0.00");
+		System.out.println("Units sold: " + sales.getUnitsSold());
+		System.out.println("Discount: $" + dollar.format(sales.getDiscount()));
+		System.out.println("Cost: $" + dollar.format(sales.getCost()));
+	}
+}
